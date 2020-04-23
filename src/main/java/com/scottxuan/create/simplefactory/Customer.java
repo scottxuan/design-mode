27ee3165d8b0c42bb77b6cee2c88e2481dc98773
zhaoxuan <zhaoxuan@jueeyaoair.com>
2020-04-23 10:27:23 +0800

    简单工厂

--- src/main/java/com/scottxuan/create/simplefactory/Customer.java
+++ src/main/java/com/scottxuan/create/simplefactory/Customer.java
@@ -11,17 +11,17 @@ public class Customer {
         Map map = PayTypeEnum.toPair();
         System.out.println(map.toString());
         //客户选择支付方式
-        int customerPayType = PayTypeEnum.WECHAT.getPayType();
-        System.out.println("客户选择:" + PayTypeEnum.getByType(customerPayType).getPayName());
+        int payType = PayTypeEnum.WECHAT.getPayType();
+        System.out.println("客户第一次选择:" + PayTypeEnum.getByType(payType).getPayName());
         //根据客户选择的支付方式
-        AbstractPay pay = PayFactory.getPay(customerPayType);
+        AbstractPay pay = PayFactory.getPay(payType);
         pay.anOrder();
 
         //客户选择支付方式
-        int payType = PayTypeEnum.ALIPAY.getPayType();
-        System.out.println("客户选择:" + PayTypeEnum.getByType(payType).getPayName());
+        int payType1 = PayTypeEnum.ALIPAY.getPayType();
+        System.out.println("客户第二次选择:" + PayTypeEnum.getByType(payType1).getPayName());
         //根据客户选择的支付方式
-        AbstractPay pay2 = PayFactory.getPay(payType);
+        AbstractPay pay2 = PayFactory.getPay(payType1);
         pay2.anOrder();
     }
 }
