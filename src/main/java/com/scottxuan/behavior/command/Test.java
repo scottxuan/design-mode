30243b34e0449ba074fe4fec8cbf94f95eb45f28
zhaoxuan <zhaoxuan@jueeyaoair.com>
2020-04-26 16:59:49 +0800

    命令模式

--- src/main/java/com/scottxuan/behavior/command/Test.java
+++ src/main/java/com/scottxuan/behavior/command/Test.java
@@ -7,5 +7,7 @@ public class Test {
     public static void main(String[] args) {
         Command command = new Command1(new Executor1());
         command.executor();
+        Command2 command2 = new Command2(new Executor2());
+        command2.executor();
     }
 }
